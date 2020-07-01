63c16444995f7a337086fc1fdf1610197d7661f1
vd89 <me@daxcodes.in>
2020-07-01 06:56:18 +0530

    add index route with registration

--- index.js
+++ index.js
@@ -1,14 +1,26 @@
 /** @format */
 
 import express from 'express';
+
 import dbController from './controller/dbController.js';
+import config from './config/default.js';
+import indexRoute from './routers/index.js';
+import expressEjsLayouts from 'express-ejs-layouts';
 
 const app = express();
+
+//Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(expressEjsLayouts);
+app.set('view engine', 'ejs');
+//Router handler
+app.use('/', indexRoute);
 
 //Db
 dbController();
 
 //server
-app.listen(8085, () => {
-	console.log(`Server is running on prot 8085`);
+app.listen(config.port, () => {
+	console.log(`Server is running on prot ${config.port}... 🤺 🤺`);
 });
